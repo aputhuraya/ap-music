67481efdbd04500c8493ef31f4ef287c80f3e2a1
Anish Puthuraya <anish.gopalakrishna@gmail.com>
2019-12-27 11:48:07 +0530

    Updates

---
+++ js/index.js
@@ -0,0 +1,41 @@
+function deferVideo() {
+  //defer html5 video loading
+  $("video source").each(function() {
+    var sourceFile = $(this).attr("data-src");
+    $(this).attr("src", sourceFile);
+    var video = this.parentElement;
+    video.load();
+    // uncomment if video is not autoplay
+  });
+  $(".carousel").carousel({
+    interval: 10000
+  });
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= $(window).height() * 0.55) {
+      $(".navbar").css("background-color", "black");
+      $(".navbar").css("opacity", "0.9");
+      $(".navbar .row div").css("opacity", "0.9");
+      $(".navbar .row div").hover(
+        function() {
+          $(this).css("opacity", "1");
+        },
+        function() {
+          $(this).css("opacity", "0.9");
+        }
+      );
+    } else {
+      $(".navbar").css("background-color", "transparent");
+      $(".navbar .row div").css("opacity", "0.5");
+      $(".navbar .row div").css("transition", "opacity 0.6s");
+      $(".navbar .row div").hover(
+        function() {
+          $(this).css("opacity", "1");
+        },
+        function() {
+          $(this).css("opacity", "0.5");
+        }
+      );
+    }
+  });
+}
+window.onload = deferVideo;
